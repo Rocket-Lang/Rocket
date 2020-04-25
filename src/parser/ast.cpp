0e300185d48578e55c1d8be4ef2e602619638ee5
fuchslenny <lenny.fuchs@outlook.de>
2020-04-25 14:05:54 +0200

    added include changes

--- src/parser/ast.cpp
+++ src/parser/ast.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-#include <Rocket/src/include/lexer/tokenizer.h>
-#include <Rocket/src/include/parser/ast.h>
 
-void make_ast(){
-    for(int i = 0; i<sizeof(list); i++){
-        std::string curr_token = list.at(i);
-        std::cout << curr_token;
+
+namespace Rocket{
+    namespace ast{
+    /*
+    void make_ast(){
+        for(int i = 0; i<sizeof(list); i++){
+            std::string curr_token = list.at(i);
+            std::cout << curr_token;
+            }
+        }
+    */
     }
+
 }
